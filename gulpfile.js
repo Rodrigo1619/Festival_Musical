2d3da0fbe79d0594aac231d80b018c4e0f455175
rodrigo1619 <00067920@uca.edu.sv>
2022-07-26 17:46:20 -0600

    watch agregado

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {src, dest} = require("gulp");
+const {src, dest, watch} = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
 
 function css(done){
@@ -12,5 +12,11 @@ function css(done){
 
     done(); //callback que avisa a gulp que se ha llegado al final
 }
+function dev(done){
+    watch('src/scss/app.scss', css)
+
+    done();
+}
 
 exports.css = css;
+exports.dev = dev;
